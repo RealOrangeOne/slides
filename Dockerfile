7885d54583b981586dc4c36b602fcc9766959cd0
Jake Howard <git@theorangeone.net>
2024-03-03 17:45:52 +0000

    Commit something which vaguely works

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM alpine:3.19
+
+RUN apk add --no-cache nginx gettext nginx-mod-http-brotli nginx-mod-http-dav-ext nginx-mod-http-js
+
+RUN mkdir -p /run/nginx
+RUN htpasswd -b -c /etc/nginx/.htpasswd user password
+
+COPY ./nginx.conf /etc/nginx/http.d/default.conf
+COPY ./slides.js /etc/nginx/slides.js
+COPY ./docker-entrypoint.sh /docker-entrypoint.sh
+
+ENV PUID 1000
+
+EXPOSE 80
+
+STOPSIGNAL SIGKILL
+
+CMD ["/docker-entrypoint.sh"]
---
+++ slides.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+function sitemap(r) {
+    const directories = fs.readdirSync("/srv")
+        .map(f => [f, fs.statSync(`/srv/${f}`)])
+        .filter(d => d[1].isDirectory())
+        .map(d => {
+            const directoryName= d[0];
+            const stats = d[1];
+            const modifiedTime = new Date(stats.mtime);
+            return `<url><loc>https://slides.theorangeone.net/${directoryName}/</loc><lastmod>${modifiedTime.toISOString().split("T")[0]}</lastmod></url>`;
+        });
+
+    const sitemapData = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+        ${directories.join('')}
+        </urlset>
+    `.replace("\n", "");
+
+    r.headersOut["content-type"] = "application/xml";
+
+    r.return(200, sitemapData);
+}
+
+export default {sitemap};
