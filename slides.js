798d0e96c04537ef4d69f3817ca3677d0a63c4c4
Jake Howard <git@theorangeone.net>
2024-03-03 21:11:41 +0000

    Dynamically get sitemap hostname

--- slides.js
+++ slides.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
 function sitemap(r) {
+    const hostname = r.variables["host"];
+
     const directories = fs.readdirSync("/srv")
         .map(f => [f, fs.statSync(`/srv/${f}`)])
         .filter(d => d[1].isDirectory())
@@ -8,7 +10,7 @@ function sitemap(r) {
             const directoryName= d[0];
             const stats = d[1];
             const modifiedTime = new Date(stats.mtime);
-            return `<url><loc>https://slides.theorangeone.net/${directoryName}/</loc><lastmod>${modifiedTime.toISOString().split("T")[0]}</lastmod></url>`;
+            return `<url><loc>https://${hostname}/${directoryName}/</loc><lastmod>${modifiedTime.toISOString().split("T")[0]}</lastmod></url>`;
         });
 
     const sitemapData = `<?xml version="1.0" encoding="UTF-8"?>
